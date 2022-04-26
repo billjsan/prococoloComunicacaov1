87697c6bfe5e6d3da9ac553e8d33c4e2fee44b86
bill <wjefferson.santos@gmail.com>
2022-04-26 14:22:21 -0300

    adição de autores da musica usada como exemplo adição de um exemplo de pagina com explicação do protocolo de marcação

--- src/App.java
+++ src/App.java
@@ -2,11 +2,13 @@
  * autor Willian J. Dos Santos
  * data 24/04/2022
  */
+
 import server.STTP1;
 
 public class App {
     public static void main(String[] args) {
 
+        // Riders on the storm - The Doors
         String dataToTransfer = "Riders on the storm " +
                 "Into this house, we're born " +
                 "Into this world, we're thrown " +
@@ -21,11 +23,22 @@ public class App {
                 "Sweet family will die " +
                 "Killer on the road, yeah";
 
-        String pageContent = "lalalalalalalal";
+        //<.start> -> inicio do documento
+        //<:end> -> fim do documento
+        //<$b> -> inicio tag de negrito
+        //<#b> -> fim tag de negrito
+        //<$i> -> inicio tag de italico
+        //<#i> -> fim tag de italico
+        //<:> -> tag de quebra de linha
+        String pageContent = "<.start> texto de exemplo<:>" +
+                "criação de um protocolo com <$b>tags<#b><:>" +
+                "para serem <$i>lidas<#i> e <$b>interpretadas<#b><:>" +
+                "no cliente<:end>";
 
-            STTP1 serverRefactoring = new STTP1(8189, 80, 70,
-                    dataToTransfer,15, pageContent);
-            serverRefactoring.startServer();
+
+        STTP1 serverRefactoring = new STTP1(8189, 80, 70,
+                dataToTransfer, 15, pageContent);
+        serverRefactoring.startServer();
 
     }
 }
